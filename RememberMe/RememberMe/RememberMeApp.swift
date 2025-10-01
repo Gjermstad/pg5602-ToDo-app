80a7d613010be9397d158aa39e3b0abe77700422
Kenneth A.B. Gjermstad <79542593+Gjermstad@users.noreply.github.com>
2025-10-02 00:06:36 +0200

    Initial commit - Start project

---
+++ RememberMe/RememberMe/RememberMeApp.swift
@@ -0,0 +1,19 @@
+//
+//  RememberMeApp.swift
+//  RememberMe
+//
+//  Created by Kenneth André Bettum Gjermstad on 01/10/2025.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct RememberMeApp: App {
+
+    var body: some Scene {
+        WindowGroup {
+            MainView()
+        }
+    }
+}
