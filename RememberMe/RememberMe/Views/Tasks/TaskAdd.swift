0db8192a86805a5fe4b41ca34670a19e8e921c0e
Kenneth A.B. Gjermstad <79542593+Gjermstad@users.noreply.github.com>
2025-10-03 00:24:40 +0200

    Created TaskAdd

---
+++ RememberMe/RememberMe/Views/Tasks/TaskAdd.swift
@@ -0,0 +1,80 @@
+//
+//  ItemAdd.swift
+//  RememberMe
+//
+//  Created by Kenneth André Bettum Gjermstad on 02/10/2025.
+//
+
+import SwiftUI
+import SwiftData
+
+struct TaskAdd: View
+{
+    @Environment(\.modelContext) private var context
+    @Environment(\.dismiss) private var dismiss
+    
+    // Henter alle tilgjengelige kategorier fra databasen
+    @Query(sort: \CategoryModel.title) var categories: [CategoryModel]
+    
+    @State private var title = ""
+    @State private var selectedCategoryTitle: String = "Default"
+    
+    var body: some View
+    {
+        NavigationStack
+        {
+            let visibleCategories = categories.filter{!$0.archived}
+            
+            Form
+            {
+                Section
+                {
+                    TextField("Tittel", text: $title)
+                }
+                
+                Picker("Velg kategori", selection: $selectedCategoryTitle)
+                {
+                    Text("Ingen kategori").tag("default")
+                    ForEach(visibleCategories)
+                    {
+                        category in
+                        Text(category.title).tag(category.title)
+                    }
+                }
+            }
+            .navigationTitle("Ny oppgave")
+            .toolbar {
+                ToolbarItem(placement: .cancellationAction)
+                {
+                    Button("Avbryt") {
+                        dismiss()
+                    }
+                }
+                ToolbarItem(placement: .confirmationAction)
+                {
+                    Button("Lagre") {
+                        let chosenCategory = categories.first{$0.title == selectedCategoryTitle}
+                        let category = chosenCategory ?? {
+                            let c = CategoryModel(title: "default")
+                            context.insert(c)
+                            return c
+                        }()
+                        
+                        let task = TaskModel(title: title, category: category)
+                        
+                        task.title = title
+                        task.category = category
+                        
+                        context.insert(task)
+                        
+                        dismiss()
+                    }
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    TaskAdd()
+}
