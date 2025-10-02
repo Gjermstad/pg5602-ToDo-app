d580d2e071714073a58274ec326d66a37c1924a4
Kenneth A.B. Gjermstad <79542593+Gjermstad@users.noreply.github.com>
2025-10-03 00:25:33 +0200

    Renamed file

---
+++ RememberMe/RememberMe/Views/Tasks/TaskView.swift
@@ -0,0 +1,20 @@
+//
+//  StartView.swift
+//  RememberMe
+//
+//  Created by Kenneth André Bettum Gjermstad on 02/10/2025.
+//
+
+import SwiftUI
+
+struct TaskView: View
+{
+    var body: some View
+    {
+        Text("Startskjermen")
+    }
+}
+
+#Preview {
+    TaskView()
+}
