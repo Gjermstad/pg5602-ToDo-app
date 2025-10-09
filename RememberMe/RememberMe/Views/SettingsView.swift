7234f83a04a05189331351ed1985001a40b2b0d3
Kenneth A. Bettum Gjermstad <79542593+Gjermstad@users.noreply.github.com>
2025-10-09 23:33:56 +0200

    Added lecturers code to show path to local database on device

--- RememberMe/RememberMe/Views/SettingsView.swift
+++ RememberMe/RememberMe/Views/SettingsView.swift
@@ -11,6 +11,8 @@ struct SettingsView: View {
     
     @AppStorage("darkmode") private var darkMode: Bool = false
     
+    @State private var path = ""
+    
     var body: some View
     {
         NavigationStack
@@ -23,6 +25,46 @@ struct SettingsView: View {
                            systemImage: darkMode ? "moon.zzz" : "moon.circle",
                            isOn: $darkMode
                     )
+                }
+                
+                Section
+                {
+                  Button
+                  {
+                    // Sjekker om path-variabelen er tom
+                    if path.isEmpty
+                    {
+                      // Hvis tom: hent filstien til dokumentmappen og lagre i path
+                      path = URL.documentsDirectory.path()
+                      print(path) // Skriver ut stien til Xcode-debug-konsollen
+                    }
+                    else
+                    {
+                      // Hvis path allerede har verdi: "tøm" den (slik at teksten skjules)
+                      path = ""
+                    }
+                  }
+                  label:
+                  {
+                    Label(path.isEmpty ? "Vis lokal database-lokasjon" : "Skjul database lokasjon", systemImage: "square.stack.3d.up.fill")
+                  }
+                  
+                  // Viser stien som tekst under knappen, men bare hvis path har verdi
+                  if !path.isEmpty
+                  {
+                    Text(path)
+                    
+                    Button
+                    {
+                      // Kopierer innholdet av variabelen til systemets utklippstavle,
+                      // slik at brukeren kan lime det inn i andre apper.
+                      UIPasteboard.general.string = path
+                    }
+                    label:
+                    {
+                      Label("Kopier til utklippstavle", systemImage: "document.on.document.fill")
+                    }
+                  }
                 }
             }
             .navigationTitle("Innstillinger")
